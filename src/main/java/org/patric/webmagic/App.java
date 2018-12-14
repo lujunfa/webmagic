ce4e234ebab70ce2ca00edabb4d3251efcc9cf84
lujunfa <2990606482@qq.com>
2018-12-14 14:40:28 +0800

    webmagic

---
+++ src/main/java/org/patric/webmagic/App.java
@@ -0,0 +1,13 @@
+package org.patric.webmagic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
